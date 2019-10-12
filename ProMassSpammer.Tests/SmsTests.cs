f8860088db675fc4958b5d4ae16a5e7ef2d67fc2
Eli Hayon <dyslexicanaboko@gmail.com>
2019-10-12 02:23:46 -0400

    Created a bad test for testing the SMS functionality. Have to do a full test using a mass communication.

---
+++ ProMassSpammer.Tests/SmsTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ProMassSpammer.Core.Configuration;
+using ProMassSpammer.Core.Transmission.Sms;
+
+namespace ProMassSpammer.Tests
+{
+    [TestFixture]
+    public class SmsTests
+    {
+        [Test]
+        public void SendSmsViaTwilio()
+        {
+            var config = Config.GetSmsConfig();
+
+            var svc = new SmsTwilioClient(config);
+
+            var msg = new SmsMessage
+            {
+                From = "+10123456789", //Replace with Twilio phone number
+                To = "+10123456789", //Replace with real phone number to test with
+                Body = "I like bacon!"
+            };
+
+            svc.Send(msg);
+        }
+    }
+}
